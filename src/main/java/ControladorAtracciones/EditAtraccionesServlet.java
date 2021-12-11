484e88ed43384cba8925a09d5a623927372e3ab0
32514243 <87551219+32514243@users.noreply.github.com>
2021-12-11 20:06:08 -0300

    correccion
    coreccion

--- src/main/java/ControladorAtracciones/EditAtraccionesServlet.java
+++ src/main/java/ControladorAtracciones/EditAtraccionesServlet.java
@@ -23,34 +23,34 @@ public class EditAtraccionesServlet extends HttpServlet {
 		super.init();
 		this.attractionService = new AtraccionService();
 	}
-
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Integer id = Integer.parseInt(req.getParameter("id"));
 
-		Atraccion attraction = attractionService.find(id);
-		req.setAttribute("attraction", attraction);
-
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/views/attractions/edit.jsp");
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/views/attractions/create.jsp");
 		dispatcher.forward(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
 		String nombre = req.getParameter("nombre");
-		Integer tipoDePromocion = Integer.parseInt(req.getParameter("tipoDePromocion"));
 		Double costo = Double.parseDouble(req.getParameter("costo"));
-		Integer descuentoPorcentual = Integer.parseInt(req.getParameter("desceuntoPorcentual"));
-		//List<Atraccion> atracciones = List<Atraccion>.parseList(req.getParameter("atracciones"));
-	
-		Promocion promocion = promocionesService.create(nombre, tipoDePromocion, costo, descuentoPorcentual, );
-		if (promocion.isValid()) {
-			resp.sendRedirect("/TP003/promociones/index.do");
+		Double duracion = Double.parseDouble(req.getParameter("duracion"));
+		Integer cupoActual = Integer.parseInt(req.getParameter("cupoActual"));
+		Integer cupoMaximo = Integer.parseInt(req.getParameter("cupoMaximo"));
+		Integer posicionX = Integer.parseInt(req.getParameter("posicionX"));
+		Integer posicionY = Integer.parseInt(req.getParameter("posicionY"));
+		Atraccion attraction = attractionService.create(nombre, costo, duracion, cupoActual, cupoMaximo, posicionX, posicionY);
+		if (attraction.isValid()) {
+			resp.sendRedirect("/TP003/attractions/index.do");
 		} else {
-			req.setAttribute("promocion", promocion);
-			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/views/attractions/edit.jsp");
+			req.setAttribute("attraction", attraction);
+
+			RequestDispatcher dispatcher = getServletContext()
+					.getRequestDispatcher("/views/attractions/edit.jsp");
 			dispatcher.forward(req, resp);
 		}
-	}
-}
 
+	}
+
+}
